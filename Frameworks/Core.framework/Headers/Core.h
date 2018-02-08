4c0d12a335534637c5c7d7ea3cbeeeaacacc780c
Pranjal Satija <pranjalsatija@users.noreply.github.com>
2018-02-07 23:56:19 -0600

    Initial Commit

---
+++ Frameworks/Core.framework/Headers/Core.h
@@ -0,0 +1,15 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by Pranjal Satija on 1/11/18.
+//  Copyright © 2018 Pranjal Satija. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+//! Project version number for Core.
+FOUNDATION_EXPORT double CoreVersionNumber;
+
+//! Project version string for Core.
+FOUNDATION_EXPORT const unsigned char CoreVersionString[];
---
+++ Frameworks/UI.framework/Headers/UI.h
@@ -0,0 +1,20 @@
+//
+//  UI.h
+//  UI
+//
+//  Created by Pranjal Satija on 1/27/18.
+//  Copyright © 2018 Pranjal Satija. All rights reserved.
+//
+
+#import <SnapKit/SnapKit.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for UI.
+FOUNDATION_EXPORT double UIVersionNumber;
+
+//! Project version string for UI.
+FOUNDATION_EXPORT const unsigned char UIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UI/PublicHeader.h>
+
+
